56cd96624a7b743867fd3eaae4ff2efc9c512d20
Vagner Fabricio <wagnerfabricio@hotmail.com>
2021-10-05 17:02:52 -0300

    Criado randomicamente o array gamePosition

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,7 @@ const botaoStart = document.getElementById("play");
 function hiddenInstrutions() {
   instrutions.classList.remove("instrutions");
   instrutions.classList.add("hidden");
+  createGamePosition(0);
   createBoard();
 }
 
@@ -37,52 +38,68 @@ botaoStart.addEventListener("click", hiddenInstrutions);
 /* ------------------------ MERO ------------------------ */
 
 /* ------------------------ MAURO ----------------------- */
-const jogador1 = 1
+const jogador1 = 1;
 
-function checarVitoria(gamePosition){
+function checarVitoria(gamePosition) {
   const edgeX = gamePosition[0].length - 2;
   const edgeY = gamePosition.length - 2;
 
   //horizontal
-  for(let y = 0; y < gamePosition.length; y++){
-    for(let x = 0; x < edgeX; x++) {
+  for (let y = 0; y < gamePosition.length; y++) {
+    for (let x = 0; x < edgeX; x++) {
       let cell = gamePosition[y][x];
-      if(cell !== 0) {
-        if(cell === gamePosition[y][x+1] && cell === gamePosition[y][x+2] && cell === gamePosition[y][x+3] ) {
-          return `Jogador ${gamePosition[y][x]} venceu `
+      if (cell !== 0) {
+        if (
+          cell === gamePosition[y][x + 1] &&
+          cell === gamePosition[y][x + 2] &&
+          cell === gamePosition[y][x + 3]
+        ) {
+          return `Jogador ${gamePosition[y][x]} venceu `;
         }
       }
     }
   }
   //vertical
-  for(let y = 0; y < edgeY; y++){
-    for(let x = 0; x < gamePosition[0].length; x++) {
+  for (let y = 0; y < edgeY; y++) {
+    for (let x = 0; x < gamePosition[0].length; x++) {
       cell = gamePosition[y][x];
-      if(cell !== 0) {
-        if(cell === gamePosition[y+1][x] && cell === gamePosition[y+2][x] && cell === gamePosition[y+3][x] ) {
-          return `Jogador ${gamePosition[y][x]} venceu `
+      if (cell !== 0) {
+        if (
+          cell === gamePosition[y + 1][x] &&
+          cell === gamePosition[y + 2][x] &&
+          cell === gamePosition[y + 3][x]
+        ) {
+          return `Jogador ${gamePosition[y][x]} venceu `;
         }
       }
     }
   }
   //diagonal 753
-  for(let y = 0; y < edgeY; y++){
-    for(let x = 0; x < edgeX; x++) {
+  for (let y = 0; y < edgeY; y++) {
+    for (let x = 0; x < edgeX; x++) {
       cell = gamePosition[y][x];
-      if(cell !== 0) {
-        if(cell === gamePosition[y+1][x+1] && cell === gamePosition[y+2][x+2] && cell === gamePosition[y+3][x+3] ) {
-          return `Jogador ${gamePosition[y][x]} venceu `
+      if (cell !== 0) {
+        if (
+          cell === gamePosition[y + 1][x + 1] &&
+          cell === gamePosition[y + 2][x + 2] &&
+          cell === gamePosition[y + 3][x + 3]
+        ) {
+          return `Jogador ${gamePosition[y][x]} venceu `;
         }
       }
     }
   }
   //diagonal 159
-  for(let y = 2; y < gamePosition.length; y++){
-    for(let x = 0; x < edgeX; x++) {
+  for (let y = 2; y < gamePosition.length; y++) {
+    for (let x = 0; x < edgeX; x++) {
       cell = gamePosition[y][x];
-      if(cell !== 0) {
-        if(cell === gamePosition[y-1][x+1] && cell === gamePosition[y-2][x+2] && cell === gamePosition[y-3][x+3] ) {
-          return `Jogador ${gamePosition[y][x]} venceu `
+      if (cell !== 0) {
+        if (
+          cell === gamePosition[y - 1][x + 1] &&
+          cell === gamePosition[y - 2][x + 2] &&
+          cell === gamePosition[y - 3][x + 3]
+        ) {
+          return `Jogador ${gamePosition[y][x]} venceu `;
         }
       }
     }
@@ -98,5 +115,17 @@ function checarVitoria(gamePosition){
 /* ----------------------- VAGNER ----------------------- */
 function captureData(elementColumn, elementLine, player) {
   gamePosition[elementColumn][elementLine] = player;
+}
+
+let gamePosition = [];
+function createGamePosition(n) {
+  gamePosition = [];
+  for (let i = 0; i < 7; i++) {
+    newColum = [];
+    for (let j = 0; j < 6; j++) {
+      newColum.push(n);
+    }
+    gamePosition.push(newColum);
+  }
 }
 /* ----------------------- VAGNER ----------------------- */
